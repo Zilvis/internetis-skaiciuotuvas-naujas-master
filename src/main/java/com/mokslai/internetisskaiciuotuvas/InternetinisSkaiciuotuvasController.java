98f2d340e445d3ce2d7f4bcf896dcd4f6d909a4d
ZilvisMainPC <zi.daugela@gmail.com>
2023-04-24 21:11:59 +0300

    Prideta validacija

--- src/main/java/com/mokslai/internetisskaiciuotuvas/InternetinisSkaiciuotuvasController.java
+++ src/main/java/com/mokslai/internetisskaiciuotuvas/InternetinisSkaiciuotuvasController.java
@@ -1,14 +1,14 @@
 package com.mokslai.internetisskaiciuotuvas;
 
+import jakarta.validation.Valid;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -39,52 +39,65 @@ public class InternetinisSkaiciuotuvasController {
     // Encoder https://meyerweb.com/eric/tools/dencoder/
 
 
-    public String skaiciuoti(@RequestParam HashMap <String , String> ivedimoSarasas, ModelMap isvedimoSarasas) {
+    public String skaiciuoti(
+            // Svarbu: parametras bindingResult turi eiti iskarto po anotacijos @Valid
+            // Kitu atveju gausite klaida " validation failed for object "
+            @Valid @ModelAttribute("number") SkaiciusZenklas num, BindingResult bindingResult,
+            @RequestParam HashMap <String , String> ivedimoSarasas, ModelMap isvedimoSarasas) {
         // Per url perduodamas raktas ( kintamasis ) turi pavadinima sk1
         // Pagal rakta sk1 istraukiama reiksme pvz ( tarkime vartotojas ivede 8 )
         // Vadinasi mums reikia konvertuoti is String i Int kad paskaiciuotumeme rezultata
-        int sk1 = Integer.parseInt(ivedimoSarasas.get("sk1"));
-        int sk2 = Integer.parseInt(ivedimoSarasas.get("sk2"));
-        String zenklas = ivedimoSarasas.get("zenklas");
 
-        double ats = 0;
-        if (zenklas.equals("+")) {
-            ats = sk1 + sk2;
-        } else if (zenklas.equals("-")) {
-            ats = sk1 - sk2;
-        } else if (zenklas.equals("*")){
-            ats = sk1 * sk2;
-        } else if (zenklas.equals("/") && sk2 != 0){
-            ats = sk1 / sk2;
-        }
+        // Jeigu validacijos klaidos ( Ziureti SkaiciusZenklas klaseje aprasyta validacija prie kiekvieno skaiciaus )
+        if (bindingResult.hasErrors()){
+            // Vartotojas lieka skaiciuotuvo lange tol, kol neistaiso validacijos klaidu.
+            return "skaiciuotuvas";
+        // Vartotojas praejo validacija - skaiciuojamas rezultatas
+        } else {
+            int sk1 = Integer.parseInt(ivedimoSarasas.get("sk1"));
+            int sk2 = Integer.parseInt(ivedimoSarasas.get("sk2"));
+            String zenklas = ivedimoSarasas.get("zenklas");
+            double rezultatas = 0;
+            if (zenklas.equals("+")) {
+                rezultatas = sk1 + sk2;
+            } else if (zenklas.equals("-")) {
+                rezultatas = sk1 - sk2;
+            } else if (zenklas.equals("*")){
+                rezultatas = sk1 * sk2;
+            } else if (zenklas.equals("/") && sk2 != 0){
+                rezultatas = sk1 / sk2;
+            }
 
-        // Ivedimo sarasas naudojamas siusti duomenis is Spring MVC controlerio i Jsp faila ( vaizda )
-        isvedimoSarasas.put("sk1", sk1);
-        isvedimoSarasas.put("sk2", sk2);
-        isvedimoSarasas.put("zenklas", zenklas);
-        isvedimoSarasas.put("rezultatas", ats);
+            // Ivedimo sarasas naudojamas siusti duomenis is Spring MVC controlerio i Jsp faila ( vaizda )
+            isvedimoSarasas.put("sk1", sk1);
+            isvedimoSarasas.put("sk2", sk2);
+            isvedimoSarasas.put("zenklas", zenklas);
+            isvedimoSarasas.put("rezultatas", rezultatas);
 
-        // Grazinamas vaizdas ( forma )
-        // Svarbu nurodyti per Aplication.properties prefix ir suffix
-        // Nes pagal tai ieskos vaizdo projekte
-        return "skaiciuoti";
-        // ApplicationContext yra interface skirtas suteikti informacija apie aplikacijos konfiguracija.
-        // Siuo atveju naudojama konfiguracija paimama is beans.xml failo
-        //ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
+            // Grazinamas vaizdas ( forma )
+            // Svarbu nurodyti per Aplication.properties prefix ir suffix
+            // Nes pagal tai ieskos vaizdo projekte
+            return "skaiciuoti";
+            // ApplicationContext yra interface skirtas suteikti informacija apie aplikacijos konfiguracija.
+            // Siuo atveju naudojama konfiguracija paimama is beans.xml failo
+            // ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
 
-        // bean - klases objektas kuris atitinka singleton sablona.
-       // HelloWorld bean = (HelloWorld) appContext.getBean("helloWorld");
-       // return bean.getHello();
+            // bean - klases objektas kuris atitinka singleton sablona.
+            // HelloWorld bean = (HelloWorld) appContext.getBean("helloWorld");
+            // return bean.getHello();
 //        return "Internetinis Skaiciuotuvas <p>" +
 //                "<b>Galimos operacijos:     </b></br>" +
 //                "&nbsp;&nbsp; sudeti    </br>"+
 //                "&nbsp;&nbsp; dauginti  </br>"+
 //                "&nbsp;&nbsp; dalinti   </br>"+
 //                "&nbsp;&nbsp; atimtis   </br></p>";
+        }
     }
     @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String rodytiPagrindiniPuslapi (){
 
+    public String rodytiPagrindiniPuslapi (Model model){
+        // Jeigu Model "number" nepraeina validacijos per ji grazinamos validacijos klaidos i View
+        model.addAttribute("number", new SkaiciusZenklas());
         // Graziname jsp faila kuris turi buti talpinamas "webapp -> WEB-INF -> JSP" aplanke
         return "skaiciuotuvas";
     }
